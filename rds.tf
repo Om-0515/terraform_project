6c1bedefb799e34a152919e166a440e4e8e430b0
Om Mandalik <om.shivaji.mandalik.com>
2026-09-07 23:22:16 +0530

    rds modification

--- rds.tf
+++ rds.tf
@@ -7,8 +7,8 @@ resource "aws_rds_cluster" "postgresql" {
   cluster_identifier     = "aurora-db-cluster"
   engine                 = "aurora-postgresql"
   database_name          = "mydb"
-  master_username        = "user-om"
-  master_password        = "om2002"
+  master_username        = "userom"
+  master_password        = "ommm2002"
   db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
    skip_final_snapshot    = true
